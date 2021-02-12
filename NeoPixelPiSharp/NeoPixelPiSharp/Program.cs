b565aaa719153ecca93323b4eb076259e3ee52d9
spanishprisoner <jacek.mowinski@gmail.com>
2021-02-12 19:51:23 +0100

    Test program

--- NeoPixelPiSharp/NeoPixelPiSharp/Program.cs
+++ NeoPixelPiSharp/NeoPixelPiSharp/Program.cs
@@ -5,10 +5,17 @@ namespace NeoPixelPiSharp
 {
     class Program
     {
+        const string script =
+            "import board" +
+            "import neopixel" +
+            "" +
+            "pixels = neopixel.NeoPixel(board.D18, 1, pixel_order = neopixel.RGBW)" +
+            "pixels.fill((0, 255, 0))";
+
         static void Main(string[] args)
         {
             ScriptEngine pythonEngine = Python.CreateEngine();
-            ScriptSource pythonScript = pythonEngine.CreateScriptSourceFromString("print 'Hello World!'");
+            ScriptSource pythonScript = pythonEngine.CreateScriptSourceFromString(script);
             pythonScript.Execute();
         }
     }
