a651a51f8d84234a729d37d759de7c9f91c33131
spanishprisoner <jacek.mowinski@gmail.com>
2021-02-12 19:08:57 +0100

    Adding IronPython

---
+++ NeoPixelPiSharp/NeoPixelPiSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace NeoPixelPiSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScriptEngine pythonEngine = Python.CreateEngine();
+            ScriptSource pythonScript = pythonEngine.CreateScriptSourceFromString("print 'Hello World!'");
+            pythonScript.Execute();
+        }
+    }
+}
